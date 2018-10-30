ff50da42821f8337068c16dea0c03f448f9d3bb1
Deeter <deeter.cesler@gmail.com>
2018-10-29 20:48:53 -0600

    done with edge first bonus edge case

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,17 +2,44 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
-  render() {
+  constructor(){
+    super();
+    this.state = {
+      balance: 0,
+    }
+  }
+  deposit = () => {
+    const amount = parseInt(this.amount.value);
+    if(!amount){
+      return;
+    }
+    this.setState({
+      balance: this.state.balance + amount
+    })
+    // empty out the text box in this component
+    this.amount.value = '';
+  }
+  withdraw = () => {
+    const amount = parseInt(this.amount.value);
+    if(!amount){
+      return;
+    }
+    this.setState({
+      balance: this.state.balance - amount
+    })
+    // empty out the text box in this component
+    this.amount.value = '';
+  }
+  render() {;
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={(input) => this.amount = input}/>
+        <input type="button" onClick={this.deposit} value="Deposit"  />
+        <input type="button" onClick={this.withdraw} value="Withdraw" />
       </div>
     )
   }
 }
-
 export default Account;
